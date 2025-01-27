e66d8e689602e4932d98e52c51fa7f3586755ff4
holim0 <holim1226@gmail.com>
2025-01-27 22:25:38 +0900

    feat: 자기소개 출력 옵션 추가 (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Command } from 'commander'
 import showPersonalDetails from './services/showPersonalDetails'
+import introduceMe from './services/introduceMe'
 
 const program = new Command()
 
-program.version('1.0.0').description('이희제의 CLI 이력서')
+program
+  .version('1.0.0')
+  .description('이희제의 CLI 이력서')
+  .option('-i, --introduce', '이희제 자기소개')
 
 program.parse(process.argv)
 
@@ -11,4 +15,8 @@ const options = program.opts()
 
 if (Object.keys(options).length === 0) {
   showPersonalDetails()
+}
+
+if (options.introduce) {
+  introduceMe()
 }
---
+++ src/utils/log.ts
@@ -0,0 +1,1 @@
+export const logInfo = console.log
