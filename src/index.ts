56c1fc9fd9481432308aa7b0aa3cd386e5d58f2a
holim0 <holim1226@gmail.com>
2025-01-27 23:25:40 +0900

    feat: 경력 사항 및 학력 사항 추가 (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Command } from 'commander'
 import showPersonalDetails from './services/showPersonalDetails'
 import introduceMe from './services/introduceMe'
-import showSkillSet from './services/showSkillSet.ts'
+import showEducation from './services/showEducation'
+import showSkillSet from './services/showSkillSet'
+import showCareer from './services/showCareer'
 
 const program = new Command()
 
@@ -9,7 +11,9 @@ program
   .version('1.0.0')
   .description('이희제의 CLI 이력서')
   .option('-i, --introduce', '이희제 자기소개')
+  .option('-c, --career', '이희제 경력 사항')
   .option('-s, --skills', '이희제 기술 스택')
+  .option('-e, --education', '이희제 학력 사항')
 
 program.parse(process.argv)
 
@@ -19,10 +23,20 @@ if (Object.keys(options).length === 0) {
   showPersonalDetails()
 }
 
-if (options.introduce) {
-  introduceMe()
-}
+// 전달된 옵션 처리
+const selectedOption = Object.keys(options)[0] // 첫 번째 전달된 옵션 가져오기
 
-if (options.skills) {
-  showSkillSet()
+switch (selectedOption) {
+  case 'introduce':
+    introduceMe()
+    break
+  case 'career':
+    showCareer()
+    break
+  case 'skills':
+    showSkillSet()
+    break
+  case 'education':
+    showEducation()
+    break
 }
